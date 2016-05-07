a78ae16ff5d42b171fbe2275790af41a3919d635
mrsWolowitz <young-fly@users.noreply.github.com>
2016-05-07 07:51:14 +0300

    Добавлена команда добавления ингредиента

--- SaladGenerator/ViewModel/MainWindowViewModel.cs
+++ SaladGenerator/ViewModel/MainWindowViewModel.cs
@@ -7,6 +7,7 @@ using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using SaladGenerator.DB;
 using SaladGenerator.Model;
+using System.Windows.Input;
 
 namespace SaladGenerator.ViewModel
 {
@@ -25,10 +26,20 @@ namespace SaladGenerator.ViewModel
         IngredientContext ctx = new IngredientContext();
 
         public List<Ingredient> Ingredients { get; set; }
+        public ICommand ClickCommand { get; set; }
+        public string NewName { get; set; }
+        public IngredientTypes NewType { get; set; }
 
         public MainWindowViewModel()
         {
+            ClickCommand = new RelayCommand(arg => _AddIngredient());
             _FillIngredients();
+        }
+
+        private void _AddIngredient()
+        {
+            ctx.Ingredients.Add(new Ingredient { Name = NewName, Type = NewType});
+            ctx.SaveChanges();
         }
 
         private void _FillIngredients()
---
+++ SaladGenerator/ViewModel/RelayCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SaladGenerator.ViewModel
+{
+    class RelayCommand : ICommand
+    {
+        public Predicate<object> CanExecudeDelegate { get; set; }
+        public Action<object> ExecuteDelegate { get; set; }
+
+        public RelayCommand(Action<object> action)
+        {
+            ExecuteDelegate = action;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (CanExecudeDelegate != null)
+                return CanExecudeDelegate(parameter);
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (ExecuteDelegate != null)
+                ExecuteDelegate(parameter);
+        }
+    }
+}
